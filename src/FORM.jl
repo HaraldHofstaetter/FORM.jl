089e2e71cada372dc8dad62b937bb6781193bf09
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2018-03-14 13:20:08 +0100

    implemented compile_fj

--- src/FORM.jl
+++ src/FORM.jl
@@ -1,6 +1,6 @@
 module FORM
 
-export call_form, compile_f, compile_fg 
+export call_form, compile_f, compile_fg, compile_fj
 
 function call_form(input::String; threads=1)
     attempts = 0
@@ -131,6 +131,114 @@ function compile_fg(fun, vars::Vector; threads=1)
         fun = replace(fun, rep, by)
     end
     compile_fg(fun, n, threads=threads)
+end
+
+
+function compile_fj(funs::Vector, n::Integer; threads=1, opt::String="O2")
+    m = length(funs)
+    funs = [replace(replace(string(fun),"[", "("), "]", ")") for fun in funs]
+    input = string("""
+Off Statistics;
+Format $(opt);
+V x;
+#define m "$(m)"
+#define n "$(n)"
+""",
+join(["Local f$(j) = $(funs[j]);\n" for j=1:m]),
+"""
+#write <> "begin"
+#write <> "  if J==nothing"
+.sort;
+I i, j;
+S u, v;
+Local H = 
+#do i=1,'m'
+    +u^'i'*f'i' 
+#enddo
+;
+B u;
+.sort
+#optimize H
+B u;
+.sort
+#do i=1,'m'
+    Local F'i'   = H[u^'i'];
+#enddo
+.sort
+#write <> "%O"
+#do i=1,'m'
+#write <> "      F('i')=%e", F'i'
+#enddo
+#write <> "  else"
+.sort;
+S k, xx;
+#do i=1,'m'
+Hide f'i';
+#enddo
+#do i=1,'m'
+#do j=1,'n'
+    Local j'i'j'j' = f'i';
+    id x('j') = xx;
+    id xx^k? = k*xx^(k-1);
+    id xx = x('j');
+    .sort
+    Hide j'i'j'j';
+#enddo
+#enddo
+.sort
+Local H = 
+#do i=1,'m'
+    +u^'i'*v^0*f'i' 
+#do j=1,'n'
+    +u^'i'*v^'j'*j'i'j'j'
+#enddo
+#enddo
+;
+B u, v;
+.sort
+#optimize H
+B u,v;
+.sort
+#do i=1,'m'
+    Local F'i'   = H[u^'i'*v^0];
+#do j=1,'n'
+    Local J'i'J'j' = H[u^'i'*v^'j'];
+#enddo
+#enddo
+.sort
+#write <> "%O"
+#write <> "    if F!=nothing"
+#do i=1,'m'
+#write <> "      F('i')=%e", F'i'
+#enddo
+#write <> "    end"
+#do i=1,'m'
+#do j=1,'n'
+#write <> "      J('i','j')=%e", J'i'J'j'
+#enddo
+#enddo
+#write <> "  end"
+#write <> "end"
+.end
+""")
+    out = call_form(input, threads=threads)
+    out = replace(replace(out,"(", "["), ")", "]")    
+    eval(parse(string("(F,J,x)->",out)))
+end    
+
+
+function compile_fj(funs::Vector, vars::Vector; threads=1, opt::String="O2")
+    m = length(funs)
+    n = length(vars)
+    funs=[string(fun) for fun in funs]
+    for j=1:n
+        rep = string(vars[j])
+        by  = string("x(",j,")")
+        for i=1:m
+            funs[i] = replace(funs[i], rep, by)
+        end
+    end
+    compile_fj(funs, n, threads=threads, opt=opt)
 end
 
 
